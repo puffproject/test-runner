045a21fd0a5e0328ff22a689c012e48719891164
OZoneGuy <alkersho@protonmail.com>
2021-07-25 23:03:43 +0300

    feat - Fixed bad const for `UnsupportedProgrammingLanguageException`
    Added missing argument to `String.format` call

--- src/main/java/com/unityTest/testrunner/exception/code/UnsupportedProgrammingLanguageException.java
+++ src/main/java/com/unityTest/testrunner/exception/code/UnsupportedProgrammingLanguageException.java
@@ -4,6 +4,6 @@ import com.unityTest.testrunner.models.PLanguage;
 
 public class UnsupportedProgrammingLanguageException extends RuntimeException {
 	public UnsupportedProgrammingLanguageException(PLanguage pLanguage) {
-		super(String.format("programming language %s is not supported."));
+		super(String.format("programming language %s is not supported.", pLanguage));
 	}
 }
