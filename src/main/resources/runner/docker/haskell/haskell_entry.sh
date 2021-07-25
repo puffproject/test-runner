534285fb37944f7bd30bef22a0d7b0a5c9d0b3fd
OZoneGuy <alkersho@protonmail.com>
2021-07-25 22:48:52 +0300

    fix - Removed resource limitations on `haskell_entry.sh`
    Commented out all `ulimit` commands in `haskell_entry.sh`
    
    There were resource problems when running the container.
    1. `-u` option was problematic
    2. The command said it requires at least 70MiB to run
    
    More investigation is required to find proper resource limits.

--- src/main/resources/runner/docker/haskell/haskell_entry.sh
+++ src/main/resources/runner/docker/haskell/haskell_entry.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 
-# Set virtual memory limit (Should be big) (in kbytes)
-ulimit -v $PF_VIR_MEM
-ulimit -Hv $PF_VIR_MEM
+# # Set virtual memory limit (Should be big) (in kbytes)
+# ulimit -v $PF_VIR_MEM
+# ulimit -Hv $PF_VIR_MEM
 
-# Set stack memory (in kbytes)
-ulimit -s $PF_STK_MEM
-ulimit -Hs $PF_STK_MEM
+# # Set stack memory (in kbytes)
+# ulimit -s $PF_STK_MEM
+# ulimit -Hs $PF_STK_MEM
 
-# Set max user processes
-ulimit -u $PF_MAX_USR
-ulimit -Hu $PF_MAX_USR
+# # Set max user processes
+# # ulimit -u $PF_MAX_USR
+# # ulimit -Hu $PF_MAX_USR
 
-# Set max size of files written by the shell (for temp storage for pytest) (in kbytes)
-ulimit -f $PF_MAX_FILE_SZ
-ulimit -Hf $PF_MAX_FILE_SZ
+# # Set max size of files written by the shell (for temp storage for pytest) (in kbytes)
+# ulimit -f $PF_MAX_FILE_SZ
+# ulimit -Hf $PF_MAX_FILE_SZ
 
-# Set max memory size (in kbytes) (Not respected for alpine)
-ulimit -m $PF_MAX_MEM
-ulimit -Hm $PF_MAX_MEM
+# # Set max memory size (in kbytes) (Not respected for alpine)
+# ulimit -m $PF_MAX_MEM
+# ulimit -Hm $PF_MAX_MEM
 
-# Set maximum number of open file descriptors
-ulimit -n $PF_MAX_FILE_DESCRIPTORS
-ulimit -Hn $PF_MAX_FILE_DESCRIPTORS
+# # Set maximum number of open file descriptors
+# ulimit -n $PF_MAX_FILE_DESCRIPTORS
+# ulimit -Hn $PF_MAX_FILE_DESCRIPTORS
 
 # do the same command for the list of given dependencies.
 
 # Build and run test file
-timeout -s TERM $PF_TIMEOUT runhaskell --ghc-arg=-package HUnit "$TEST_FILE_NAME"
+timeout -s TERM $PF_TIMEOUT stack ghc "$TEST_FILE_NAME" -- -o test
+timeout -s TERM $PF_TIMEOUT ./test
