f34b026c7cdf7d3283c2086cdcad7d4dd81fa4fd
OZoneGuy <alkersho@protonmail.com>
2021-08-14 09:59:03 +0300

    feat - Re-enabled and set `ulimit` values
    Removed `-u` option, doesn't work in container
    Increased $PF_VIR_MEM, required for compilation
    Increased $PF_MAX_FILE_SZ, required for compilation/linking
    
    NOTE: We will probably need to increase $PF_VIR_MEM for prod, bigger programs
    might require more memory to compile

--- src/main/resources/runner/docker/haskell/haskell_entry.sh
+++ src/main/resources/runner/docker/haskell/haskell_entry.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
 
 # # Set virtual memory limit (Should be big) (in kbytes)
-# ulimit -v $PF_VIR_MEM
-# ulimit -Hv $PF_VIR_MEM
+ulimit -v $PF_VIR_MEM
+ulimit -Hv $PF_VIR_MEM
 
 # # Set stack memory (in kbytes)
-# ulimit -s $PF_STK_MEM
-# ulimit -Hs $PF_STK_MEM
-
-# # Set max user processes
-# # ulimit -u $PF_MAX_USR
-# # ulimit -Hu $PF_MAX_USR
+ulimit -s $PF_STK_MEM
+ulimit -Hs $PF_STK_MEM
 
 # # Set max size of files written by the shell (for temp storage for pytest) (in kbytes)
-# ulimit -f $PF_MAX_FILE_SZ
-# ulimit -Hf $PF_MAX_FILE_SZ
+ulimit -f $PF_MAX_FILE_SZ
+ulimit -Hf $PF_MAX_FILE_SZ
 
-# # Set max memory size (in kbytes) (Not respected for alpine)
-# ulimit -m $PF_MAX_MEM
-# ulimit -Hm $PF_MAX_MEM
+# Set max memory size (in kbytes) (Not respected for alpine)
+ulimit -m $PF_MAX_MEM
+ulimit -Hm $PF_MAX_MEM
 
 # # Set maximum number of open file descriptors
-# ulimit -n $PF_MAX_FILE_DESCRIPTORS
-# ulimit -Hn $PF_MAX_FILE_DESCRIPTORS
+ulimit -n $PF_MAX_FILE_DESCRIPTORS
+ulimit -Hn $PF_MAX_FILE_DESCRIPTORS
 
 # do the same command for the list of given dependencies.
 
